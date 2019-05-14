36fe892eb7f3f00254f38cdaec56fcdfc8e6aa55
Greg Griffin <griffin.ge@gmail.com>
2019-05-14 14:36:49 -0700

    comment about source of app.config vs. Config

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from config import Config
 
 app = Flask(__name__)
+# app.config.from_object is from flask framework, "Config" is what's imported
 app.config.from_object(Config)
 
 from app import routes
