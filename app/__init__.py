2a6a9a81e6b5c6d5e9dfb9f6f1ad2fa8efa6d8b5
Greg Griffin <griffin.ge@gmail.com>
2019-05-15 21:41:19 -0700

    add sqlalchemy config info

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 # app.config.from_object is from flask framework, "Config" is what's imported
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-from app import routes
+from app import routes, models
