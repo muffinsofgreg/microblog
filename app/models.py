178a435572937e4121166158f2758fe8ea4ca7f3
Greg Griffin <griffin.ge@gmail.com>
2019-05-16 23:15:47 -0700

    fixed typo

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@ from app import db
 
 
 class User(db.Model):
-    id = db.Column(db.Integerm primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(sb.String(120), index=True, unique=True)
     password_hash = db.Column(db.string(128))
